b8c40ab72ca23c09f8092890248050aab6b50e96
GasparMon <imoncivaez@gmail.com>
2023-11-09 00:45:39 +0100

    DOM Practicas

---
+++ Colours - Proyecto DOM/colours.js
@@ -0,0 +1,28 @@
+const boton = document.getElementById("boton-color");
+const color = document.getElementById("color")
+
+function randomColour(){
+
+    let digitos = "0123456789ABCDEF"
+    let colores = "#"
+
+    for(let i = 0; i < 6; i++){
+
+        let indiceRandom = Math.floor(Math.random() * 16);
+
+        colores += digitos[indiceRandom];
+    }
+
+    return colores;
+}
+
+boton.addEventListener("click", function(){
+
+    let bodyColour = randomColour();
+
+    color.innerText = bodyColour;
+    boton.style.backgroundColor = bodyColour;
+
+
+    document.body.style.backgroundColor = bodyColour;
+})
